0596b89ebac568ebff15ec62ea430f32f3c922b3
Kevin Justal <kjustal@uby-group.com>
2023-10-17 21:28:51 +0200

    [CONFIG] Setup vue3 with three.js

---
+++ src/components/3D/Game.vue
@@ -0,0 +1,48 @@
+<template>
+  <div ref="game" class="game"></div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import * as THREE from 'three'
+
+let game = ref(null)
+
+onMounted(() => {
+  console.log(game.value)
+
+  const scene = new THREE.Scene()
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+  const renderer = new THREE.WebGLRenderer()
+  renderer.setSize(window.innerWidth / 2, window.innerHeight / 2)
+  game.value.appendChild(renderer.domElement)
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1)
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+  const cube = new THREE.Mesh(geometry, material)
+  scene.add(cube)
+
+  camera.position.z = 5
+
+  function animate() {
+    requestAnimationFrame(animate)
+
+    cube.rotation.x += 0.01
+    cube.rotation.y += 0.01
+
+    renderer.render(scene, camera)
+  }
+
+  animate()
+})
+</script>
+
+<style>
+.game {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup>
+import Game from '../components/3D/Game.vue'
+</script>
+
+<template>
+  <Game />
+</template>
