f8ddaeb14b2838ee35e98eb48896e796602782bd
Kevin Justal <kjustal@uby-group.com>
2023-10-19 11:16:34 +0200

    [FEATURE] Adding control and lines

--- src/components/3D/Game.vue
+++ src/components/3D/Game.vue
@@ -12,7 +12,11 @@ onMounted(() => {
   console.log(game.value)
 
   const scene = new THREE.Scene()
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 500)
+  camera.position.set(0, 0, 500)
+  camera.lookAt(0, 0, 0)
+
+  console.log(camera.getFilmWidth())
 
   const renderer = new THREE.WebGLRenderer()
   renderer.setSize(window.innerWidth / 2, window.innerHeight / 2)
@@ -21,19 +25,52 @@ onMounted(() => {
   const geometry = new THREE.BoxGeometry(1, 1, 1)
   const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
   const cube = new THREE.Mesh(geometry, material)
+  cube.name = 'player'
+  cube.position.set(0, -camera.getFilmHeight() / 4, 490)
   scene.add(cube)
-
-  camera.position.z = 5
+  drawLines(scene, camera)
 
   function animate() {
     requestAnimationFrame(animate)
 
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
-
     renderer.render(scene, camera)
   }
 
+  function drawLines(scene, camera) {
+    const material = new THREE.LineBasicMaterial({ color: 0x0000ff })
+
+    const points = []
+    points.push(new THREE.Vector3(camera.getFilmWidth() / 2, -camera.getFilmHeight() / 2, 500))
+    points.push(new THREE.Vector3(30, -camera.getFilmHeight() / 2, 0))
+    points.push(new THREE.Vector3(-30, -camera.getFilmHeight() / 2, 0))
+    points.push(new THREE.Vector3(-camera.getFilmWidth() / 2, -camera.getFilmHeight() / 2, 500))
+
+    const geometry = new THREE.BufferGeometry().setFromPoints(points)
+    const line = new THREE.Line(geometry, material)
+    scene.add(line)
+  }
+
+  function setupKeyControls(scene) {
+    var cube = scene.getObjectByName('player')
+    document.onkeydown = function (e) {
+      switch (e.keyCode) {
+        case 37:
+          cube.position.x -= 0.1
+          break
+        case 38:
+          cube.position.z -= 0.1
+          break
+        case 39:
+          cube.position.x += 0.1
+          break
+        case 40:
+          cube.position.z += 0.1
+          break
+      }
+    }
+  }
+
+  setupKeyControls(scene)
   animate()
 })
 </script>
