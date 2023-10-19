b870ce08df0db277b354c63245f8d9de01947c1b
justalk <kjustal@uby-group.com>
2023-10-19 11:25:51 +0200

    [FEATURE] Removing console.log

--- src/components/3D/Game.vue
+++ src/components/3D/Game.vue
@@ -9,8 +9,6 @@ import * as THREE from 'three'
 let game = ref(null)
 
 onMounted(() => {
-  console.log(game.value)
-
   const scene = new THREE.Scene()
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 500)
   camera.position.set(0, 0, 500)
