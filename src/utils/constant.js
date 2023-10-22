a7c962ff7ca0563f7b26f346ddcf977b3ca60420
Justal Kevin <justal.kevin@gmail.com>
2023-10-22 19:55:08 +0200

    [REFACTORING] Create the class line for helping managing the lines

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -5,9 +5,20 @@ export const KEYBOARD_KEY = {
   DOWN: 40
 }
 
+export const CAMERA = {
+  POSITION_Z: 500,
+  TARGET_POSITION_Z: 0
+}
+
 export const PLAYER = {
   NAME: 'player',
+  POSITION_Z: 480,
   SIZE: 2,
   ACCELERATION: 200,
   IMPULSE: 0.9
+}
+
+export const COLOR = {
+  RED: 0xff0000,
+  BLUE: 0x0000ff
 }
