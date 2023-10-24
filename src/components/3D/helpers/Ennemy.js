746bee851383e9f4d090de8e42a97349e4a3bc82
Justal Kevin <justal.kevin@gmail.com>
2023-10-24 21:46:36 +0200

    [FEATURE] Randomize ennemy

--- src/components/3D/helpers/Ennemy.js
+++ src/components/3D/helpers/Ennemy.js
@@ -1,10 +1,12 @@
 import * as THREE from 'three'
-import { COLOR, ENNEMY } from '../../../utils/constant'
+import { CAMERA, COLOR, ENNEMY } from '../../../utils/constant'
 
 export default class Ennemy {
   constructor(scene, camera) {
     this.scene = scene
     this.camera = camera
+    this.maxPositionX = this.camera.getFilmWidth() / 4
+    this.minPositionX = -this.maxPositionX
   }
 
   init() {
@@ -12,20 +14,25 @@ export default class Ennemy {
     const material = new THREE.MeshBasicMaterial({ color: COLOR.PINK, side: THREE.DoubleSide })
     const plane = new THREE.Mesh(geometry, material)
     plane.name = ENNEMY.NAME
-    plane.position.set(-this.camera.getFilmWidth() / 4, -this.camera.getFilmHeight() / 4, 0)
+    plane.position.set(-this.getRandomPositionX(), -this.camera.getFilmHeight() / 4, 0)
     this.scene.add(plane)
+  }
+
+  getRandomPositionX() {
+    return Math.floor(Math.random() * (this.maxPositionX - this.minPositionX + 1) + this.minPositionX)
   }
 
   reset() {
     const plane = this.scene.getObjectByName(ENNEMY.NAME)
     plane.position.z = 0
+    plane.position.x = this.getRandomPositionX()
   }
 
   tick(delta) {
     const plane = this.scene.getObjectByName(ENNEMY.NAME)
     plane.position.z += ENNEMY.SPEED * delta 
 
-    if (plane.position.z > 500) {
+    if (plane.position.z > CAMERA.POSITION_Z) {
         this.reset()
     }
   }
